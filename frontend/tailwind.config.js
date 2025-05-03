61066ba2d42731b0b0f9f1593172f411ccf1d3ba
Mateo Balzola <mateobalzola@Mateos-MacBook-Pro.local>
2025-05-03 19:06:54 -0400

    added all json files and configed them and got local host working

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',            // use .dark on <html>
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: { extend: {} },
+    plugins: [require('@tailwindcss/forms')],
+  };
+  
+
+  
